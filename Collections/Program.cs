de7954907bb6ad869af3d7b0a285945d3e1ab4e6
Umut Unal <ce.umutunal@gmail.com>
2021-02-25 23:57:04 +0300

    Generic Intro Updated

---
+++ Collections/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Umut", "Lana", "Ruh", "Lale" };
+
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //Console.WriteLine("-_-_-_-_-_-_-_");
+            
+
+            //isimler = new string[5];
+            //isimler[4] = "Murtaza";
+
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> {"Umut", "Lana", "Ruh", "Lale" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Recai");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+        }
+    }
+}
---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        //whenever we renew the class, constructor will work.
+        T[] items;
+        //constructor
+        public MyList()
+        {
+            items = new T[0];   
+         
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items; //Here we will protect our last know data, otherwise every time we call new items it would lost last date.
+            items = new T[items.Length + 1]; //By this way we make our code dynamic, and we will increase it every time we call the constructor.
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i]; //one by one items get back its last know dates from tempArray.
+
+            }
+                items[items.Length - 1] = item;
+
+            
+        }
+    }
+}
